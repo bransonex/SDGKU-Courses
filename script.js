a0195b3ba64def9399816733eb71f590dbed47ab
bransonex <72634035+bransonex@users.noreply.github.com>
2021-10-07 21:09:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var userName="Kvon"
+var email="Kvon.Smith@gmail.com"
+var password="hidden"
+var age="29"
+var dateofBirth="January 18, 1992"
+var gender="male"
+var nationality="American"
+var race="Cherokee Indian"
+var date="10/7/2021"
+var time="8:40 pm"
+var occupation="Software Entrepreneur"
+var salary="120000"
+var education="Bachelors degree"
+var university="Interamerican University of Puerto Rico"
+var currentCity="Atlanta"
+var phoneNumber="470-855-3005"
+var country="United States"
+var employer="Self Employed"
+var homeAddress="Windy Hill Rd"
+var zipCode="30067"
+
+document.write(`Hello ${userName}`);
+
+document.write(`${email}`);
+
+document.write(`${password}`);
+
+document.write(`${age}`);
+
+document.write(`${dateofBirth}`);
+
+document.write(`${gender}`);
+
+document.write(`${nationality}`);
+
+document.write(`${race}`);
+
+document.write(`${date}`);
+
+document.write(`${time}`);
+
+document.write(`${time}`);
+
+document.write(`${occupation}`);
+
+document.write(`${salary}`);
+
+document.write(`${education}`);
+
+document.write(`${university}`);
+
+document.write(`${currentCity}`);
+
+document.write(`${phoneNumber}`);
+
+document.write(`${country}`);
+
+document.write(`${employer}`);
+
+document.write(`Your home address is ${homeAddress}`);
+
+document.write(`${zipCode}`);
