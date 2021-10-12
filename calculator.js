1952d1a0aaa5ec9c584b1773ceb0ad0222e12d72
bransonex <72634035+bransonex@users.noreply.github.com>
2021-10-12 19:50:08 -0400

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,8 @@
+function calculator(){
+    let num1=Number(prompt("Enter your number 1: "));
+    let num2=Number(prompt("Enter your number 2: "));
+    console.log(num1,num2);
+    document.getElementById("results").innerHTML= <p> ${num1} + ${num2} = ${num1 + num2}</p>
+
+    ;
+}
