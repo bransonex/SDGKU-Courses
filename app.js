2a1def52b2731bbe821a684d1c9a29f81bd26a66
bransonex <72634035+bransonex@users.noreply.github.com>
2021-11-03 17:10:15 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function init(){
+    console.log("Hello World!");
+
+let btn = document.getElementById("btn save");
+
+sayHello();
+console.log("Kvon")
+}
+window.onload =  init;
+
+function sayHello(){
+
+}
